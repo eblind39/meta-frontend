65ee383e960bea526b11c736a17fe8c28205c19f
Ernesto Gutierrez <ergutierrez39@gmail.com>
2024-12-09 15:20:59 -0600

    feat: js intro

---
+++ react_intro/intro.js
@@ -0,0 +1,37 @@
+console.log('%cHello World!', "color: blue; font-size: 18px;");
+
+console.log("Hello " + "there, " + "World")
+
+// This is a comment
+
+/*
+This
+is
+a
+multi-line
+comment
+*/
+
+/* This is a multi-line comment in a single line */
+
+let greeting = "Hello";
+let person = "Jhon";
+console.info(greeting, person);
+
+greeting = 'Hi';
+person = 'Jim';
+console.log(greeting, person);
+
+// JS module01 - lab01
+var petDog = 'Rex';
+var petCat = 'Pepper';
+console.log(petDog);
+console.log(petCat);
+console.log("My pet dog's name is: ", petDog);
+console.log("My pet cat's name is: ", petCat);
+var catSound = 'purr';
+var dogSound = 'woof';
+console.log(petDog + ' says ' + dogSound);
+console.log(petCat + ' says ' + catSound);
+catSound = 'meow';
+console.log(petCat + ' now says ' + catSound)
