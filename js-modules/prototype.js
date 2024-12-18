69497dbd4a58f9d1f8a64948f26427087877f28f
Ernesto Gutierrez <ergutierrez39@gmail.com>
2024-12-18 14:36:28 -0600

    feat: default parameters

--- js-modules/prototype.js
+++ js-modules/prototype.js
@@ -116,3 +116,37 @@ let boxingGym = new Gym('7-22', 'right corner', 'left corner');
 console.log(boxingGym.openHrs);
 console.log(boxingGym.stationeryBike);
 console.log(boxingGym.treadmill);
+
+// ES6 default parameters
+function withDefaultParams(number = 10) {
+    if (isNaN(number)) {
+        console.log('Invalid input, a number is required.');
+        return;
+    }
+    console.log('Result:', number * number);
+}
+
+withDefaultParams();
+withDefaultParams(5);
+withDefaultParams('NaN');
+
+class WithDefaultParams {
+    constructor(num1 = 1, num2 = 2, num3 = 3, string1 = 'Result:', bool1 = true) {
+        this.num1 = num1;
+        this.num2 = num2;
+        this.num3 = num3;
+        this.string1 = string1;
+        this.bool1 = bool1;
+    }
+
+    calculate() {
+        if (this.bool1) {
+            console.log(this.string1, this.num1 + this.num2 + this.num3);
+            return;
+        }
+        return 'The value of bool1 is incorrect';
+    }
+}
+
+let betterWDP = new WithDefaultParams();
+betterWDP.calculate();
