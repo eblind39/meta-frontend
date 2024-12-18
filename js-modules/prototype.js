992d33447263484510140906057bb3aa42c81b10
Ernesto Gutierrez <ergutierrez39@gmail.com>
2024-12-18 07:47:38 -0600

    feat: classes advanced

--- js-modules/prototype.js
+++ js-modules/prototype.js
@@ -27,4 +27,92 @@ class Train {
         this.color = color;
         this.lightsOn = lightsOn;
     }
+
+    toggleLights() {
+        this.lightsOn = !this.lightsOn;
+    }
+
+    lightsStatus() {
+        console.log('Lights on?', this.lightsOn);
+    }
+
+    getSelf() {
+        console.log('Self:', this);
+    }
+
+    getPrototype() {
+        var proto = Object.getPrototypeOf(this);
+        console.log('Train prototype:', proto);
+    }
 }
+
+Train.prototype.printClassName = function() {
+    console.log('Class name:', 'Train')
+}
+
+let myFirstTrain = new Train('red', false);
+let mySecondTrain = new Train('blue', false);
+let myThirdTrain = new Train('black', false);
+console.log(myFirstTrain);
+console.log(mySecondTrain);
+console.log(myThirdTrain);
+myFirstTrain.toggleLights();
+myFirstTrain.lightsStatus();
+myFirstTrain.getSelf();
+myFirstTrain.getPrototype();
+myFirstTrain.printClassName();
+
+class HighSpeedTrain extends Train {
+    constructor(passengers, highSpeedOn, color, lightsOn) {
+        super(color, lightsOn);
+        this.passengers = passengers;
+        this.highSpeedOn = highSpeedOn;
+    }
+
+    toggleHighSpeed() {
+        this.highSpeedOn = !this.highSpeedOn;
+        console.log('High speed status:', this.highSpeedOn);
+    }
+
+    toggleLights() {
+        super.toggleLights();
+        super.lightsStatus();
+        console.log('Lights are 100% operational.');
+    }
+}
+
+let train5 = new Train('blue', false);
+let highSpeed1 = new HighSpeedTrain(200, false, 'green', false);
+train5.toggleLights();
+train5.lightsStatus();
+train5.getPrototype();
+highSpeed1.getPrototype();
+console.log(HighSpeedTrain.prototype.__proto__);
+
+class StationaryBike {
+    constructor(position, gears) {
+        this.position = position;
+        this.gears = gears;
+    }
+}
+
+class Treadmill {
+    constructor(position, modes) {
+        this.position = position;
+        this.modes = modes;
+    }
+}
+
+class Gym {
+    constructor(openHrs, stationeryBikePos, treadmillPos) {
+        this.openHrs = openHrs;
+        this.stationeryBike = new StationaryBike(stationeryBikePos, 8);
+        this.treadmill = new Treadmill(treadmillPos, 5);
+    }
+}
+
+let boxingGym = new Gym('7-22', 'right corner', 'left corner');
+
+console.log(boxingGym.openHrs);
+console.log(boxingGym.stationeryBike);
+console.log(boxingGym.treadmill);
