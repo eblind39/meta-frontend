630b49fc2710c0ed81a829be6b8d1869c1b9e8c3
Ernesto Gutierrez <ergutierrez39@gmail.com>
2024-12-24 21:21:26 -0600

    feat: version control, local vs. remote

---
+++ vcontrol/gitlocalremote.sh
@@ -0,0 +1,13 @@
+mkdir my-second-repo
+cd my-second-repo
+git init
+git remote -v
+git remote add origin git@github.com:GitTutorials101/my-first-repo.git
+git remote -v
+git pull
+git checkout main
+git status
+git push origin main
+cat test.txt
+git pull
+cat test.txt
