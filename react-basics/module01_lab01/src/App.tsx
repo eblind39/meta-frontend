ce936e21ffefe562f63f66cad39ca64b24ccae54
Ernesto Gutierrez <ergutierrez39@gmail.com>
2025-01-31 17:16:56 -0600

    feat: react, adding additional route

--- react-basics/module01_lab01/src/App.tsx
+++ react-basics/module01_lab01/src/App.tsx
@@ -1,6 +1,7 @@
 import HomePage from './HomePage';
 import RegisterForm from './RegisterForm';
 import MealsView from './MealsView';
+import Contact from './Contact';
 import { Routes, Route, Link } from 'react-router';
 import './css/card.css';
 
@@ -14,12 +15,14 @@ function App({title = ''}: Props = {}) {
       <nav>
         <Link to="/">Home</Link><span>/</span>
         <Link to="/register">Register</Link><span>/</span>
-        <Link to="/meals">Meals</Link>
+        <Link to="/meals">Meals</Link><span>/</span>
+        <Link to="/contact" className="nav-item">Contact</Link>
       </nav>
       <Routes>
         <Route path="/" element={<HomePage title={title} />} />
         <Route path="/register" element={<RegisterForm />} />
         <Route path="/meals" element={<MealsView />} />
+        <Route path="/contact" element={<Contact />}></Route>
       </Routes>
     </div>
   );
---
+++ react-basics/module01_lab01/src/Contact.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+const Contact = () => React.createElement('h1', null, "Contact Little Lemon on this page");
+
+export default Contact;
