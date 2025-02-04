181389663e863897cdf391cf4b6c2a305670ef7d
Ernesto Gutierrez <ergutierrez39@gmail.com>
2025-02-04 16:53:08 -0600

    feat: react adv, memo & useMemo

---
+++ react-advanced/react-app/src/components/memo/UseMemo.tsx
@@ -0,0 +1,25 @@
+import React, { useContext, useMemo } from "react";
+
+const AppContext = React.createContext({a: "", b: ""});
+
+// A good rule of thumb is to wrap the React component right after your context provider with React.memo
+const UseMemo = () => {
+    const a = 'hi';
+    const b = 'bye';
+    const value = useMemo(() => ({a, b}), [a, b]);
+
+    return (
+        <AppContext.Provider value={value}>
+            <ComponentA />
+        </AppContext.Provider>
+    )
+}
+
+const ComponentA = React.memo(() => <ComponentB />);
+const ComponentB = () => <ComponentC />;
+const ComponentC = () => {
+    const contextValue = useContext(AppContext);
+    return null;
+};
+
+export default UseMemo;
