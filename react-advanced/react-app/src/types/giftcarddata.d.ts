135807623025e5b73ccac4eeede15c3689ddc647
Ernesto Gutierrez <ergutierrez39@gmail.com>
2025-02-05 10:14:35 -0600

    feat: react adv, gift card, useState

---
+++ react-advanced/react-app/src/types/giftcarddata.d.ts
@@ -0,0 +1,9 @@
+interface GiftCardData {
+    firstName: string
+    lastName: string
+    text: string
+    valid: boolean
+    instructions: string
+}
+  
+export default GiftCardData;
--- react-advanced/react-app/src/views/Hooks.tsx
+++ react-advanced/react-app/src/views/Hooks.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import UseStateObj from "../components/hooks/UseStateObj";
 import Goals from "../components/hooks/Goals";
+import GiftCard from "../components/hooks/GiftCard";
 
 const Hooks = () => {
     return (
         <React.Fragment>
             <UseStateObj />
             <Goals />
+            <GiftCard />
         </React.Fragment>
     )
 }
