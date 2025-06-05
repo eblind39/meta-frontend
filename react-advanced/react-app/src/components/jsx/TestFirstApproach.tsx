0ca967c1ca3f2b3e0fe3bad58aba5d83efa5b9e2
Ernesto Gutierrez <ergutierrez39@gmail.com>
2025-06-05 11:46:17 -0600

    feat: test, improve first test

--- react-advanced/react-app/src/components/jsx/TestFirstApproach.tsx
+++ react-advanced/react-app/src/components/jsx/TestFirstApproach.tsx
@@ -1,7 +1,15 @@
+import { useEffect, useState } from "react";
+
 const TestFirstApproach = () => {
+    const [title, setTitle] = useState<string>("");
+
+    useEffect(() => {
+        setTitle("Little Lemon Restaurant");
+    }, [])
+
     return (
         <div className="contruncontr-container">
-            <a href="https://littlelemon.com/" className="app-link">Little Lemon Restaurant</a>
+            <a href="https://littlelemon.com/" className="app-link">{title}</a>
         </div>
     )
 }
--- react-advanced/react-app/src/tests/testing.spec.tsx
+++ react-advanced/react-app/src/tests/testing.spec.tsx
@@ -6,7 +6,9 @@ describe('Testing example Component', () => {
     it('should have a link', () => {
         render(<Testing />);
         const linkElement = screen.getByRole('link', { name: /little lemon restaurant/i });
+        // console.log(linkElement.innerHTML);
         expect(linkElement).toBeInTheDocument();
         expect(linkElement).toHaveAttribute('href', 'https://littlelemon.com/');
+        expect(linkElement.innerHTML).toEqual('Little Lemon Restaurant');
     })
 })
