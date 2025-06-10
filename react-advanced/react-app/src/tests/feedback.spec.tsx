3d0c4e844161d550200db16a27d2914a0349e3c0
Ernesto Gutierrez <ergutierrez39@gmail.com>
2025-06-10 16:14:52 -0600

    refactor: test, renaming test

--- react-advanced/react-app/src/tests/feedback.spec.tsx
+++ react-advanced/react-app/src/tests/feedback.spec.tsx
@@ -46,14 +46,16 @@ describe("FeedbackForm Component", () => {
         // expect(submitButton).toHaveAttribute("disabled");
     });
 
-    test("User is able to submit the form if the score is higher than 5, without additional feedback", () => {
+    it("User is able to submit the form if the score is higher than 5, without additional feedback", () => {
         const score = "9";
         const comment = "";
         const handleSubmit = jest.fn();
+
         handleSubmit({
             score,
             comment,
         });
+        
         render(<FeedbackForm onSubmit={handleSubmit} />);
 
         // You have to write the rest of the test below to make the assertion pass
